0d1e68ff9446e754be53d75d0107f150c1cef7ae
Josh Braegger <jbraegger@backcountry.com>
2012-03-23 13:48:00 -0600

    Using setuptools when possible

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
-from distutils.core import setup
+#!/usr/bin/env python
+
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from distutils.command.install_data import install_data
+
+class osx_install_data(install_data):
+    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
+    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
+    # for this in distutils.command.install_data#306. It fixes install_lib but not
+    # install_data, which is why we roll our own install_data class.
+
+    def finalize_options(self):
+        # By the time finalize_options is called, install.install_lib is set to the
+        # fixed directory, so we set the installdir to install_lib. The
+        # install_data class uses ('install_data', 'install_dir') instead.
+        self.set_undefined_options('install', ('install_lib', 'install_dir'))
+        install_data.finalize_options(self)
+
+if sys.platform == "darwin":
+    cmdclasses = {'install_data': osx_install_data}
+else:
+    cmdclasses = {'install_data': install_data}
 
 args = {
     'name': 'git-crucible',
@@ -13,6 +38,7 @@ args = {
     'license': 'BSD',
     'packages': ["crucible"],
     'scripts': ["bin/git-crucible"],
+    'cmdclass': cmdclasses,
     'install_requires': ['restkit'],
     'classifiers': [
         'Programming Language :: Python',
