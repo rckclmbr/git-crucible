d5ace30452245c1e27b0cfb3fde7249b3178b3ff
Josh Braegger <jbraegger@backcountry.com>
2012-03-25 21:50:17 -0600

    Adding 'git show' commands and tests

--- crucible/git.py
+++ crucible/git.py
@@ -1,8 +1,38 @@
 import popen2
+import subprocess
 
 def diff(command):
-    r, w, e = popen2.popen3("git diff '%s'" % command)
-    error = "".join(e)
+    pr = subprocess.Popen(['/usr/bin/git', 'diff', command],
+           stdout=subprocess.PIPE,
+           stderr=subprocess.PIPE,
+           )
+    (out, error) = pr.communicate()
     if error:
         raise Exception(error)
-    return "".join(r)
+    return out
+
+def show(command):
+    pr = subprocess.Popen(['/usr/bin/git', 'show', command],
+           stdout=subprocess.PIPE,
+           stderr=subprocess.PIPE,
+           )
+    (out, error) = pr.communicate()
+    if error:
+        raise Exception(error)
+    commits = []
+    from collections import defaultdict
+    commit = defaultdict(lambda: "")
+    ignoretext = False
+    for line in out.split("\n"):
+        if line.startswith("commit "):
+            if commit["commit"]:
+                commits.insert(0, commit)
+                commit = defaultdict(lambda: "")
+            commit["commit"] = line[7:]
+            ignoretext = True
+        elif line.startswith("diff "):
+            ignoretext = False
+        if not ignoretext:
+            commit["patch"] += line + "\n"
+    commits.insert(0, commit)
+    return commits
--- tests/test_git.py
+++ tests/test_git.py
@@ -7,9 +7,15 @@ import os
 
 
 class GitTest(unittest.TestCase):
+
+    def setUp(self):
+        self.curr = os.getcwd()
+        os.chdir(os.path.join(os.path.dirname(__file__), "data/repo"))
+
+    def tearDown(self):
+        os.chdir(self.curr)
     
     def test_diff(self):
-        os.chdir(os.path.join(os.path.dirname(__file__), "data/repo"))
         res = git.diff("HEAD^..")
         self.assertEquals(res, """diff --git a/test2 b/test2
 index 180cf83..84a746b 100644
@@ -19,8 +25,8 @@ index 180cf83..84a746b 100644
  test2
 +test3
 """)
+
     def test_diff_error(self):
-        os.chdir(os.path.join(os.path.dirname(__file__), "data/repo"))
         try:
             res = git.diff("HEAD^^^..")
         except Exception, e:
@@ -28,3 +34,13 @@ index 180cf83..84a746b 100644
                 """revision or path not in the working tree.\nUse '--' to separate paths from revisions\n""")
         else:
             assert False, "Didn't raise an exception"
+
+    def test_show(self):
+        res = git.show("HEAD^^..")
+        self.assertEquals(len(res), 2)
+        self.assertEquals(res[0]["commit"], "970c00a734bae26c459c09e9d346b3457fdf8e40")
+        self.assertEquals(res[0]["patch"], "diff --git a/test2 b/test2\nnew file mode 100644\n"
+            "index 0000000..180cf83\n--- /dev/null\n+++ b/test2\n@@ -0,0 +1 @@\n+test2\n\n")
+        self.assertEquals(res[1]["commit"], "9cca389d388e75eda4e07d4a0e0eba078a08e3dc")
+        self.assertEquals(res[1]["patch"], "diff --git a/test2 b/test2\nindex 180cf83..84a746b 100644\n"
+            "--- a/test2\n+++ b/test2\n@@ -1 +1,2 @@\n test2\n+test3\n\n")
